f60d8d7e279240b07b91d6391931aa36aeb8abae
naiping1113 <244105997@qq.com>
2019-07-12 08:42:30 +0800

    基础数仓

--- src/main/scala/Spark_DM/DWD_TO_DM_USER_BASIC.scala
+++ src/main/scala/Spark_DM/DWD_TO_DM_USER_BASIC.scala
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory
 
 object DWD_TO_DM_USER_BASIC {
   def main(args: Array[String]): Unit = {
+    System.setProperty("HADOOP_USER_NAME","root")
     val conf = SparkSession
       .builder()
       .enableHiveSupport()
@@ -14,21 +15,19 @@ object DWD_TO_DM_USER_BASIC {
       .master(SparkConstants.Constant.SPARK_LOCAL)
       .getOrCreate()
 
-    val sql = ConfigManager.getProper(args(0))
+    val sql = ConfigManager.getProper("qfbap_dm.dm_user_basic")
     if (sql == null) {
       LoggerFactory.getLogger("SparkLogger")
         .debug("提交的表名参数异常!请重新设置!")
     }else{
-      val finalsql = sql.replace("?",args(1))
-      val df = conf.sql(finalsql)
-      val mysqlTableName = args(0).split("\\.")(1)
-      val hiveTableName = args(0)
+      val df = conf.sql(sql)
+      val mysqlTableName = "dm_user_basic"
 
       val jdbcProp = JDBCUtils.getjdbcProp()._1
       val jdbcUrl = JDBCUtils.getjdbcProp()._2
 
       df.write.mode(SaveMode.Append).jdbc(jdbcUrl,mysqlTableName,jdbcProp)
-      df.write.mode(SaveMode.Append).insertInto(hiveTableName)
+      //df.write.mode(SaveMode.Append).insertInto(hiveTableName)
     }
   }
 }
